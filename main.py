7ef927eaf2f6ca4f4f5e2bcff3d6f9764493dcd1
khadada <lkhadada@gmail.com>
2021-11-22 09:17:41 +0100

    I create ball module and I create to paddle bar in my game with control keys for each paddle 'z/s' and 'Up/Down'

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 from turtle import Screen
-from time import sleep
+from paddle import Paddle,LEFT_SIDE,RIGHT_SIDE
+from ball import Ball
 
 my_screen = Screen()
 my_screen.title('Pong game created by Khaled')
 my_screen.setup(800, 600)
+my_screen.listen()
+paddle_left = Paddle(LEFT_SIDE)
+paddle_right = Paddle(RIGHT_SIDE)
+my_screen.onkey(paddle_right.move_up, 'Up')
+my_screen.onkey(paddle_right.move_down, 'Down')
+my_screen.onkey(paddle_left.move_up, 'z')
+my_screen.onkey(paddle_left.move_down, 's')
+ball = Ball()
+
+my_screen.exitonclick()
---
+++ paddle.py
@@ -0,0 +1,19 @@
+from turtle import Turtle
+LEFT_SIDE = -380
+RIGHT_SIDE = 380
+class Paddle(Turtle):
+    def __init__(self,side):
+        super().__init__()
+        self.shape('square')
+        self.color("black")
+        self.penup()
+        self.shapesize(5, 1)
+        self.setx(side)
+
+    def move_up(self):
+        if self.ycor() < 240:
+            self.goto(self.xcor(), self.ycor()+20)
+
+    def move_down(self):
+        if self.ycor() > -240:
+            self.goto(self.xcor(), self.ycor()-20)
